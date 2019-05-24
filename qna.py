d083d3e4f23e0bacdbffae66c7cc5624c57e5139
cheyoon <50975993+cheyoon@users.noreply.github.com>
2019-05-24 15:02:47 +0900

    Add files via upload

---
+++ qna.py
@@ -0,0 +1,35 @@
+import discord
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    print('login')
+    print(client.user.name)
+    print(client.user.id)
+    print('------------------')
+    game = discord.Game('건의 확인중')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!문의') and not message.content.startswith("!문의확인"):
+        content = message.content[4:]
+        author = message.author
+        file = open("문의.txt", "r")
+        c = file.read()
+        file.close()
+        file = open("문의.txt", "w")
+        file.write(c + "\n" + author.name + "(" + str(author.id) + ") : " + content)
+        file.close()
+        await message.delete()
+        await message.channel.send(author.name + "님의 문의 접수가 완료되었습니다.")
+
+    if message.content.startswith("!문의확인") and (message.author.id == 463243589703434250 or message.author.id == 366221416967700483):
+        file = open("문의.txt", "r")
+        c = file.read()
+        await message.channel.send(c)
+
+client.run('NTgxMTMwNjQ4OTc5MjQzMDI5.XOdpNg.NY_zsphz26B6o0ZTjSTqAXWalfE')
